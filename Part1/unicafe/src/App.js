b9f13568936237ddd9e00f7b7a2096e6597457a9
spandan <spandan999nepal@gmail.com>
2021-04-17 00:01:05 +0530

    1.8: unicafe step 3

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react';
 
+const Statistics = ({good, neutral, bad}) => {
+    const total = good + neutral + bad
+    const average = total / 3
+    const positive = (good / total) * 100
+    return (
+        <>
+        <h2>Statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {average}</p>
+        <p>positive {isNaN(positive)? 0: positive + " %"}</p>
+        </>
+    ) 
+}
+
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-    const total = good + neutral + bad
-    const average = total / 3
-    const positive = (good / total) * 100
-    
     return(
         <div>
             <h1>Give Feedback</h1>
@@ -21,14 +34,7 @@ const App = () => {
             <button onClick={() => setBad(bad + 1)}>
                 bad
             </button>
-
-            <h2>Statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {total}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
