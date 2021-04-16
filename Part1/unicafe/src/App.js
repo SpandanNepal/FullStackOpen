b20bf4ab880b9cf8423f43fb6ecfef88db8d8bf3
spandan <spandan999nepal@gmail.com>
2021-04-16 23:37:30 +0530

    1.7: unicafe step 2

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -4,6 +4,10 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+
+    const total = good + neutral + bad
+    const average = total / 3
+    const positive = (good / total) * 100
     
     return(
         <div>
@@ -17,10 +21,14 @@ const App = () => {
             <button onClick={() => setBad(bad + 1)}>
                 bad
             </button>
+
             <h2>Statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
         </div>
     )
 }
