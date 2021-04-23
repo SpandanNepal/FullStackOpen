374ca48c3156790da0aeeeb7ffe8a893947bd088
spandan <spandan999nepal@gmail.com>
2021-04-23 14:34:35 +0530

    2.12: Data for countries, step1

---
+++ Part2/countries/src/App.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const CountryName = ({country}) => {
+  return(
+    <h3>
+      {country.name}
+    </h3>
+  )
+}
+
+const Languages = ({languages}) => {
+  return(
+    languages.map(language => 
+      <li key={language.name}>Name: {language.name}, Native-Name: {language.nativeName}</li>
+    )
+  )
+}
+
+const SingleCountryDisplay = ({country}) => {
+  return(
+    <div>
+      <h1>
+        {country.name}
+      </h1>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <h2>Languages</h2>
+      <ul>
+        <Languages languages={country.languages}/>
+      </ul>
+      <img height='100px' width='150px' alt={country.name} src={country.flag} /> 
+    </div>
+  )
+}
+
+const CountryList = ({countries, newCountry, loading}) => {
+  const filteredCountry = () => {
+    return countries.filter(country => 
+      country.name.slice(0, newCountry.length).toLowerCase() === newCountry.toLowerCase())
+  }
+
+  if(loading){
+    return(
+      <p>loading...</p>
+    )
+  }
+  else if (filteredCountry().length === 1){
+    return(
+      <SingleCountryDisplay key={filteredCountry()[0].name} country={filteredCountry()[0]}/>
+    )
+  }
+  else if (filteredCountry().length > 10){
+    return(
+      <h2>Too many matches, specify another filter </h2>
+    )
+  }
+  else if (filteredCountry().length <= 10){
+    return(
+      filteredCountry().map(country =>
+        <CountryName key={country.name} country={country}/>
+      )
+    )
+  }
+ 
+}
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [countries, setCountries] = useState([])
+  const [newCountry, setNewCountry] = useState('Nepal')
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+    setCountries(response.data)
+    setLoading(false)
+    })
+  }, [])
+
+  
+  const handleCoutnryChange = (event) => {
+    setNewCountry(event.target.value)
+  }
+
+  return(
+    <div>
+      find countries: <input value={newCountry} onChange={handleCoutnryChange}/>
+      <CountryList countries={countries} newCountry={newCountry} loading={loading}/>
+    </div>
+  )
+}
+
+export default App;
