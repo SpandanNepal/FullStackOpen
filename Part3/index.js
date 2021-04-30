feb6c14b745f2753577ca56af98fae88c5976184
spandan <spandan999nepal@gmail.com>
2021-04-30 18:23:43 +0545

    3.5 phonebook backend step5

--- Part3/index.js
+++ Part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
     "name": "Arto Hellas", 
@@ -56,6 +58,14 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const randId = Math.floor(Math.random() * 100)
+    const person = request.body
+    person.id = randId
+    persons = persons.concat(person)
+    response.json(persons)
 })
 
 const PORT = 3001
